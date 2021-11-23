57a506634103d33db134bfb5e295468165e4305d
Melonvin <1176891456@qq.com>
2021-11-23 15:44:56 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+const nav = require('./nav')
+module.exports = {
+  title: '踩过的坑',
+  description: 'Just playing around',
+  base: "/",
+  themeConfig: {
+    logo: '',
+    nav: nav,
+    displayAllHeaders: true,
+    lastUpdated: 'Last Updated'
+  },
+  plugins: [
+    ["vuepress-plugin-auto-sidebar", {
+      sidebarDepth: 2,
+      collapse: true
+    }]
+  ]
+}
---
+++ docs/.vuepress/nav.js
@@ -0,0 +1,7 @@
+module.exports = [
+  { text: 'Home', link: '/' },
+  { text: 'Vue', link: '/vue/怎么说' },
+  { text: 'JavaScript', link: '/javascript/' },
+  { text: 'CSS', link: '/css/' },
+  { text: 'GitHub', link: 'https://github.com/Melonvin' }
+]
